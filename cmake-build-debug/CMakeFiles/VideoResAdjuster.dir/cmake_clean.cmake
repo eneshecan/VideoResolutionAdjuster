b0418b0386e93a7e65ba281fb9003e7b1a5df3e1
eneshecan <nshecan@gmail.com>
2017-11-26 03:44:36 +0300

    General decoder structure created by using FFMpeg library and samples

---
+++ cmake-build-debug/CMakeFiles/VideoResAdjuster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoResAdjuster.dir/main.cpp.o"
+  "VideoResAdjuster.pdb"
+  "VideoResAdjuster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoResAdjuster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
